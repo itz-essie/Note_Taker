e8e9f1559c0379b69a9ef725cab75115c344aa51
Esiena Ekwofia <eekwofia@gmail.com>
2021-02-01 23:25:36 -0500

    removed unnecessary package

--- server.js
+++ server.js
@@ -9,15 +9,14 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-
-app.listen(PORT, function(){
-    // npm run start- server watch
-    console.log("App is listening on PORT: " + PORT);
+app.listen(PORT, function () {
+  // npm run start- server watch
+  console.log("App is listening on PORT: " + PORT);
 });
